2439a6312a404850f3cbbbb33d37f020d54fc1b2
inzamam <malikasinger@gmail.com>
2015-06-11 09:46:16 +0500

    login page created

--- app.js
+++ app.js
@@ -3,9 +3,23 @@
  * Created by 192.168.3.50 on 6/11/2015.
  */
 
-var app = angular.module("myapp",["ngMaterial"],function($scope){
+var app = angular.module("myapp",["ngMaterial"]);
+app.controller("myctrl",function($scope){
 
 
+
+    $scope.studentlogintab = function(){
+
+        $scope.studentloginshow = true;
+        $scope.teacherloginshow = false;
+
+
+    };
+    $scope.teacherlogintab = function(){
+
+        $scope.teacherloginshow = true;
+        $scope.studentloginshow = false;
+    };
 
 
 
