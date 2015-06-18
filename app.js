d25dc17b3340a0a8ced833263e6b6832ced9de50
inzamam <malikasinger@gmail.com>
2015-06-19 01:19:19 +0500

    angular new router applied

--- app.js
+++ app.js
@@ -3,10 +3,19 @@
  * Created by 192.168.3.50 on 6/11/2015.
  */
 
-var app = angular.module("myapp",["ngMaterial"]);
-app.controller("myctrl",function($scope){
 
+    var app = angular.module('app', ['ngNewRouter','ngMaterial']);
+        app.controller('AppController', ['$router', AppController]);
 
+    AppController.$routeConfig = [
+        { path: '/',           component: 'home' },
+        { path: '/about',           component: 'about' },
+        { path: '/contact',           component: 'contact' },
+        { path: '/detail/:id', component: 'detail' }
+    ];
+    function AppController ($router) {}
+
+/*
 
     $scope.studentlogintab = function(){
 
@@ -20,8 +29,9 @@ app.controller("myctrl",function($scope){
         $scope.teacherloginshow = true;
         $scope.studentloginshow = false;
     };
+*/
 
 
 
-});
+
 
