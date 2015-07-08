d27b66f0b8a28aca7674f2506f6306129d949a53
malikasinger <malikasinger@gmail.com>
2015-07-08 12:18:33 -0700

    a service made for data storage and distributing

--- app.js
+++ app.js
@@ -5,22 +5,50 @@
 
 
     var app = angular.module('app', ['ngNewRouter','ngMaterial',
-    'app.home','app.about','app.add','app.contact']);
+    'app.home','app.add','app.student']);
 
         app.controller('AppController', ['$router', AppController]);
 
     AppController.$routeConfig = [
         { path: '/',        component: 'home' },
-        { path: '/about',      component: 'about' },
-        { path: '/contact',    component: 'contact' },
         { path: '/add',    component: 'add' },
+        { path: '/student',    component: 'student' },
         { path: '/detail/:id', component: 'detail' }
     ];
 
     function AppController ($router)
     {
 
-    }
+    };
+
+    app.service("dataservice",function(){
+
+        var teacher = [
+            {
+                name: "Ishaq Bhojani",
+                username : "admin",
+                password : "password",
+                student : [
+                    {
+                        name : "Inzamam Malik",
+                        roll : "2225",
+                        email : "malikasinger@gmail.com",
+                        password : "password",
+                        class : "mass training",
+                        task : [
+                            {
+                                taskstring : "make a webpage using angular",
+                                done : "true"
+                            }
+                        ]
+                    }
+                ]
+
+            }
+        ]
+
+
+    });
 
 
 
--- components/about/about.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by 192.168.3.50 on 6/19/2015.
- */
-angular.module('app.about', [])
-    .controller('AboutController', function () {
-        this.name = 'Friend';
-    });
